01e861acc0754414ae2dfe656a7b2b966b1d6d68
MicaelNeves <126700176+MicaelNeves@users.noreply.github.com>
2023-03-01 14:04:49 -0300

    Add files via upload

---
+++ Pasta/Aula 01/atvidades.py
@@ -0,0 +1,41 @@
+# EXEMPLO 01
+'''numero = input("Digite um numero qualquer: ")
+print(f"O numero informado foi {numero}")'''
+
+# EXEMPLO 03
+'''
+item1 = input("Digite o item 1: ")
+item2 = input("Digite o item 2: ")
+item3 = input("Digite o item 3: ")
+item4 = input("Digite o item 4: ")
+item5 = input("Digite o item 5: ")
+
+numero1 = input("Digite um numero para item 1: ")
+numero2 = input("Digite um numero para item 2: ")
+numero3 = input("Digite um numero para item 3: ")
+numero4 = input("Digite um numero para item 4: ")
+numero5 = input("Digite um numero para item 5: ")
+
+#print(f''' ''')
+
+print("======================================================")
+print(f"Item 1 {item1.upper()} - Quantidade: {numero1}")
+print(f"Item 2 {item2.upper()}  - Quantidade: {numero2}")
+print(f"Item 3 {item3.upper()} - Quantidade: {numero3}")
+print(f"Item 4 {item4.upper()} - Quantidade: {numero4}")
+print(f"Item 5 {item5.upper()} - Quantidade: {numero5}")
+print("======================================================")
+'''
+
+# EXEMPLO 04
+'''
+a = 1
+b = (-2)**(1/2)
+
+c = True
+d = 10<20
+print(d)
+print(type(a))
+print(type(b))'''
+
+
---
+++ Pasta/Aula 01/main.py
@@ -0,0 +1,35 @@
+def Hello():
+
+    msg = "hello world"
+    print(msg.upper())
+
+# STARTING
+'''
+Variavel é minuscala e a segunda maiuscula
+ex: helloWorld()
+minhaVariavelTemEsseNome = "Alguém"
+minha_variavel_tem_esse_nome = "Algo"
+'''
+
+Hello()
+'''
+msg1 = "hello"
+msg2 = "World"
+msg3 = " Micael"
+
+print(msg1+" "+msg2+" "+msg3)
+print(msg1,msg2,msg3)
+print(f"{msg1} {msg2} {msg3}") #RECOMENDADA PARA USO ESSA SINTAXE
+print("{} {} {}".format(msg1,msg2,msg3))
+'''
+
+
+nome = input("Digite seu nome: ")
+#curso = input("Digite seu curso: ")
+
+#print(f"Olá, {nome} seja bem-vindo. Seu curso é {curso}")
+print(f"{nome[5]}{nome[4]}{nome[3]}{nome[2]}{nome[1]}{nome[0]}")
+
+
+
+
