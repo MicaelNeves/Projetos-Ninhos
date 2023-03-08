4e16441a512b02b076f984f41927abc9d0c78461
MicaelNeves <126700176+MicaelNeves@users.noreply.github.com>
2023-03-08 10:33:33 -0300

    Update Exerc.py

--- Pasta/Aula 07/Exerc.py
+++ Pasta/Aula 07/Exerc.py
@@ -101,6 +101,34 @@ while True:
     else:
         break
 
+# Exemplo 07
+
+
+def valorPagamento(valor, dias_atraso):
+    if dias_atraso == 0:
+        return valor
+    elif dias_atraso != 0 and dias_atraso > 0:
+        return valor + (valor)*0.03 + (valor)*(0.01)*(dias_atraso)
+    else:
+        return print("Erro na execução do programa")
+
+
+while True:
+
+    valor = float(input("Digite o valor da prestação: "))
+    dias_atraso = int(input("Digite a quantidade de dias em atraso: "))
+    print(f"O valor a ser pago é de R$ {valorPagamento(valor,dias_atraso):,.2f}")
+
+    if valor != 0:
+        pass
+    elif not valor.isnumeric():
+        print("Digite um valor de prestação numerico")
+    elif valor < 0:
+        print("Digite um valor de prestação positivo")
+    else:
+        break
+
+
 # Exemplo 08
 
 def sequencia_8(valor):
@@ -110,6 +138,74 @@ def sequencia_8(valor):
 a = input("Digite um numero qualquer: ")
 print(sequencia_8(a))
 
+# Exemplo 09
+
+import os
+os.system('cls')
+
+
+def reverso(valor):
+    valor1 = str(valor)[::-1]
+    return valor1
+
+
+while True:
+    try:
+        valor = int(input("Digite um valor inteiro: "))
+    except:
+        print("Digite um numero natural")
+    else:
+        print(f"O reverso é {reverso(valor)}")
+        break
+     
+# Exemplo 10
+
+import random
+
+def jogar_dados():
+    dado_1 = random.randint(1, 6)
+    dado_2 = random.randint(1, 6)
+    print(f'Primeiro dado: {dado_1}')
+    print(f'Segundo dado: {dado_2}')
+    return dado_1 + dado_2
+
+rodada = 1
+ponto = 0
+validador = False
+while True:
+    iniciar = input('Desejar lançar um par de dados? [S]Sim [N]não: ')
+    iniciar = iniciar.upper()
+    if iniciar == 'S':
+        soma = jogar_dados()
+        if rodada == 1:
+            if soma == 7 or soma == 11:
+                print('Você é um "natural" e ganhou')
+                break
+            elif soma == 2 or soma == 3 or soma == 12:
+                print('Craps - Você Perdeu')
+                break
+            elif soma == 4 or soma == 5 or soma == 6 or soma == 8 or soma == 9 or soma == 10:
+                print(f'{soma} este é seu ponto')
+                ponto = soma
+                print('Seu objetivo agora é continuar jogando os dados até tirar este número novamente.')
+            rodada += 1
+        else:
+            if ponto == soma:
+                validador = True
+                print('Tirou o seu ponto')
+                print('Agora já pode tirar 7 pra ganhar')
+                rodada += 1
+            elif (validador == False) and soma == 7:
+                print('Você perdeu')
+                rodada += 1
+                break
+            elif (validador == True) and soma == 7:
+                print('Você ganhou')
+            else:
+                 print('Seu objetivo agora é continuar jogando')
+
+    else:
+        break
 
 # Exemplo 11
 
